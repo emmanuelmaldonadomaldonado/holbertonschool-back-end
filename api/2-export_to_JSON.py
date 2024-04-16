c4afd2865dcf58df2c24229ca36e1fd2350c19b7
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-04-16 10:27:44 -0400

    api 2,3

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,39 @@
+import json
+import requests
+import sys
+
+def fetch_employee_todo_list(employee_id):
+    url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("Error:", response.text)
+        return None
+    
+    data = response.json()
+    employee_name = data[0]['userId']  # Assuming 'userId' contains the employee's name
+    
+    completed_tasks = []
+    for task in data:
+        if task['completed']:
+            completed_tasks.append(task)
+
+    return {'employee_name': employee_name, 'tasks': completed_tasks}
+
+def export_to_json(employee_id, data):
+    if not data:
+        return
+    
+    filename = f"{employee_id}.json"
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    todo_list = fetch_employee_todo_list(employee_id)
+    if todo_list:
+        export_to_json(employee_id, todo_list)
+        print(f"Data exported to {employee_id}.json")
---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,40 @@
+import json
+import requests
+
+def fetch_all_employees_todo_list():
+    url = 'https://jsonplaceholder.typicode.com/todos'
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("Error:", response.text)
+        return None
+
+    data = response.json()
+    all_employees_tasks = {}
+
+    for task in data:
+        user_id = str(task['userId'])
+        task_info = {
+            'username': task['userId'],
+            'task': task['title'],
+            'completed': task['completed']
+        }
+        if user_id in all_employees_tasks:
+            all_employees_tasks[user_id].append(task_info)
+        else:
+            all_employees_tasks[user_id] = [task_info]
+
+    return all_employees_tasks
+
+def export_to_json(data):
+    if not data:
+        return
+    
+    filename = "todo_all_employees.json"
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+if __name__ == "__main__":
+    all_employees_tasks = fetch_all_employees_todo_list()
+    if all_employees_tasks:
+        export_to_json(all_employees_tasks)
+        print("Data exported to todo_all_employees.json")
