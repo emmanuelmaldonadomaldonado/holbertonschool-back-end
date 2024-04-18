6022a9e1f68d63d66fa8b86b08c4c7ffd25d1059
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-04-18 10:19:31 -0400

    .

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Script to use a REST API for a given employee ID, returns
-information about his/her list progress"""
+informaon about his/her list progress"""
 import requests
 import sys
 
